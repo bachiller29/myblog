5c0b858d1bae07ff8fb3f87b945ea34c73393d9a
bachiller29 <61086223+bachiller29@users.noreply.github.com>
2022-05-03 14:52:33 -0500

    Swagger
    Se documentan las apis

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,15 +1,21 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import {CreatePostDto, EditPostDto} from './dtos'
 import { PostService } from './post.service';
 
+@ApiTags('Posts')
 @Controller('post')
 export class PostController {
 
     constructor(private readonly postService: PostService){}
 
     @Get()
-    getMany() {
-     return this.postService.getMany()
+    async getMany() {        
+        const data = await this.postService.getMany()
+        return {
+            message: 'Peticion correcta',
+            data
+        }
     }
 
     @Get(':id')
@@ -17,26 +23,26 @@ export class PostController {
         return this.postService.getOne(id)
     }
 
-@Post()
-createOne(
-    @Body() dto: CreatePostDto
-){
-    return this.postService.createOne(dto)
-}
+    @Post()
+    createOne(
+        @Body() dto: CreatePostDto
+    ){
+        return this.postService.createOne(dto)
+    }
 
-@Put(':id')
-editOne(
-    @Param('id') id: number,
-    @Body() dto: EditPostDto
-    )
-    {
-        return this.postService.editOne(id, dto)
-}
+    @Put(':id')
+    editOne(
+        @Param('id') id: number,
+        @Body() dto: EditPostDto
+        )
+        {
+            return this.postService.editOne(id, dto)
+    }
 
-@Delete(':id')
-deleteOne(@Param('id') id: number){
+    @Delete(':id')
+    deleteOne(@Param('id') id: number){
 
-    return this.postService.deleteOne(id)
-}
+        return this.postService.deleteOne(id)
+    }
 
 }
