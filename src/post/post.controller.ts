92433b71d6abf3ff49ada455d333ea1977f72f9d
bachiller29 <61086223+bachiller29@users.noreply.github.com>
2022-05-03 09:55:20 -0500

    Services
    Se crean servicios

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,37 +1,42 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import {CreatePostDto, EditPostDto} from './dtos'
+import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
 
+    constructor(private readonly postService: PostService){}
+
     @Get()
     getMany() {
-     return 'OK';
+     return this.postService.getMany()
     }
 
     @Get(':id')
-    getOne(@Param('id') id: string){
-        console.log(id)
-        return {
-            message: 'getOne'
-        }
+    getOne(@Param('id', ParseIntPipe) id: number){        
+        return this.postService.getOne(id)
     }
 
 @Post()
-createone(
-    @Body('title') title: any,
-    @Body('content') content: any
+createOne(
+    @Body() dto: CreatePostDto
 ){
-
+    return this.postService.createOne(dto)
 }
 
 @Put(':id')
-editOne(@Param('id') id: string){
-
+editOne(
+    @Param('id') id: number,
+    @Body() dto: EditPostDto
+    )
+    {
+        return this.postService.editOne(id, dto)
 }
 
 @Delete(':id')
-delete(@Param('id') id: string){
+deleteOne(@Param('id') id: number){
 
+    return this.postService.deleteOne(id)
 }
 
 }
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostController } from './post.controller';
+import { PostService } from './post.service';
 
 @Module({
-  controllers: [PostController]
+  imports: [
+    TypeOrmModule
+  ],
+  controllers: [PostController],
+  providers: [PostService]
 })
 export class PostModule {}
